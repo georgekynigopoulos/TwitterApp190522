a22afe56980a93f70cdce5a15544e72a74205125
GEORGIOS KYNIGOPOULOS <gkynigopoulos@nyc.gr>
2022-05-19 19:14:34 +0300

    added a filtered stream with 1 filter:java

--- TwitterApp190522/Program.cs
+++ TwitterApp190522/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Tweetinvi;
 using Tweetinvi.Models;
 
@@ -6,12 +7,25 @@ namespace TwitterApp190522
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
             var userCredentials = new TwitterCredentials("CONSUMER_KEY", "CONSUMER_SECRET", "ACCESS_TOKEN", "ACCESS_TOKEN_SECRET");
             var userClient = new TwitterClient(userCredentials);
+
+
+            // Create a simple stream containing only tweets with the keyword France
+
+            var stream = userClient.Streams.CreateFilteredStream();
+            stream.AddTrack("java");
+
+            stream.MatchingTweetReceived += (sender, eventReceived) =>
+            {
+                Console.WriteLine(eventReceived.Tweet);
+            };
+
+            await stream.StartMatchingAnyConditionAsync();
 
         }
     }
